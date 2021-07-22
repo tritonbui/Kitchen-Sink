0e57e058ef2c21c24e555c17559b4e9d7a17f19a
Garmacon <luke.greenwood18@gmail.com>
2021-07-23 00:46:54 +0800

    Water Current Mechanic Testing
    Nothing added that works yet,
    but a method for the water current mechanic is being devised and underway.

--- PowerSink/Assets/scripts/blockScripts/waterCurrentScript.cs
+++ PowerSink/Assets/scripts/blockScripts/waterCurrentScript.cs
@@ -5,4 +5,42 @@ using UnityEngine;
 public class waterCurrentScript : MonoBehaviour
 {
     public Transform lerpDestination;
+    public GameObject player;
+    public Rigidbody _rb;
+    public float speed;
+
+    public void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            if (player == null)
+            {
+                player = col.gameObject;
+            }
+
+            player.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            player.GetComponent<Rigidbody>().useGravity = true;
+            player = null;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        if (player != null)
+        {
+            player.transform.position = Vector3.Lerp(player.transform.position, lerpDestination.position, Time.deltaTime * speed);
+        }
+    }
 }
